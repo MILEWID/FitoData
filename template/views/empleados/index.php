943efa044741a137d51928435e87e2ec60639297
MILEWID <milewid@gmail.com>
2021-05-19 14:45:42 -0500

    login

--- template/views/empleados/index.php
+++ template/views/empleados/index.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/login.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Chela+One&display=swap" rel="stylesheet">
-    <title>Document</title>
-</head>
+<?php require_once INCLUDES . 'header.php' ?>
 <body>
     <!-- nav bar para el inicio de Seción -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class ="container">
            <div class="col-1">
-            <img src="../img/logo.png" class="navbar-brand" href="#">></img>
+            <img src= "<?php echo IMAGES."logo.png"?>" class="navbar-brand" href="#">></img>
            </div>
            <div class="col-7">
             <h2 class=>FitoData</h2>
